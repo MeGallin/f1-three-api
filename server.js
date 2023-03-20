bb5530af1fc86555c73b30d58a958593fc6689f0
MeGallin <me@garyallin.uk>
2023-03-20 21:14:06 +0000

    mongo setup

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('dotenv').config({ path: './config.env' });
 const cors = require('cors');
-
+const connectDB = require('./config/db');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
